c4858f95e3d58b5dc7df9dc1c97f9cd33b8937eb
kuba <kuba@MacBook-Air-kuba.local>
2023-02-03 15:21:08 +0100

    initial commit

---
+++ components/FeaturesPanel.js
@@ -0,0 +1,98 @@
+import styles from '@/styles/Panels.module.scss'
+
+export const FeaturesPanel = () => {
+    return <div className={`${styles.Panel} ${styles.Panel__features}`}>
+        <div className={`${styles.Card} ${styles['Card--no-bottom-padding']}`}>
+            <div className={styles.Card__badge}>
+                Promocje
+            </div>
+            <h3 className={styles.Card__title}>
+                Otrzymuj powiadomienia o promocjach na bieżąco
+            </h3>
+            <h4 className={styles.Card__subtitle}>
+                Skanujemy setki stron dziennie najpopularniejszych sklepów, aby dostarczyć naszym użytkownikom wyłącznie promocje
+            </h4>
+            <img src='phone.png' className={styles.Card__phone}></img>
+        </div>
+        <div className={styles.Card}>
+            <div className={styles.Card__badge}>
+                Promocje
+            </div>
+            <h3 className={styles.Card__title}>
+                W Promly są tylko promocje
+            </h3>
+            <h4 className={styles.Card__subtitle}>
+                Promly obsługuje największe sklepy z ubraniami w polskim internecie takie jak:
+            </h4>
+            <div className={styles.Card__logos}>
+                <span>
+                    <img src='nike.svg' />
+                </span>
+                <span>
+                    <img src='adidas.svg' />
+                </span>
+                <span>
+                    <img src='zalando.svg' />
+                </span>
+                <span>
+                    <img src='ws2.svg' />
+                </span>
+            </div>
+            <div className={styles['Card__logos--text']}>
+                <span>+ Zalando, Nike, Adidas, Snipes, Asos, Eastend, Footshop, Zalando, WS2</span>
+            </div>
+        </div>
+        <div className={`${styles.Card} ${styles['Card--no-padding']}  ${styles.Card__column}`}>
+            <div className={`${styles.Card__row}`}>
+                <div className={styles.Card__row__image}>
+                    <img src='selection.svg' />
+                </div>
+                <div className={styles.Card__row__content}>
+                    <h3 className={styles.Card__title}>
+                        W Promly są tylko promocje
+                    </h3>
+                    <h4 className={styles.Card__subtitle}>
+                        Promly obsługuje największe sklepy z ubraniami w polskim internecie takie jak:
+                    </h4>
+                </div>
+            </div>
+            <div className={`${styles.Card__row} ${styles['Card__row--reverse']} $`}>
+                <div className={styles.Card__row__content}>
+                    <h3 className={styles.Card__title}>
+                        W Promly są tylko promocje
+                    </h3>
+                    <h4 className={styles.Card__subtitle}>
+                        Promly obsługuje największe sklepy z ubraniami w polskim internecie takie jak:
+                    </h4>
+                </div>
+                <div className={`${styles.Card__row__image} ${styles['Card__row__image--reverse']}`}>
+                    <img src="flash.svg"></img>
+                </div>
+            </div>
+            <div className={`${styles.Card__row}`}>
+                <div className={styles.Card__row__image}>
+                    <img src='discount.svg' />
+                </div>
+                {/* <img rc='discount.svg' className={styles.Card__row__image} /> */}
+                <div className={styles.Card__row__content}>
+                    <h3 className={styles.Card__title}>
+                        W Promly są tylko promocje
+                    </h3>
+                    <h4 className={styles.Card__subtitle}>
+                        Promly obsługuje największe sklepy z ubraniami w polskim internecie takie jak:
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div className={styles.Card}>
+            <div className={styles.Card__badge}>
+                Oszczędność
+            </div>
+            <h3 className={styles.Card__title}>
+                Z Promly oszczędasz średnio 30-40% na jednym kupowanym produkcie
+            </h3>
+        </div>
+
+        <span className={styles.Copyright}>© 2021 - 2023 Promly - All Rights Reserved.</span>
+    </div>
+}
---
+++ components/HeadingPanel.js
@@ -0,0 +1,25 @@
+import styles from '@/styles/Panels.module.scss'
+import Image from 'next/image'
+
+export const HeadingPanel = () => {
+    return <div className={`${styles.Panel} ${styles.Panel__heading}`}>
+        <h1>Wszystkie promocje streetwearowe w jednym miejscu — Promly</h1>
+        <div className={styles.Panel__heading__buttons}>
+            <a href='#' alt='Download Promly on Android'>
+                <img src='play-store-badge.svg' alt='Play Store' />
+            </a>
+            <a href='#' alt='Download Promly on iOS'>
+                <img src='app-store-badge.svg' alt='App Store' style={{ opacity: 0.5 }} />
+
+            </a>
+            <span style={{ fontFamily: "Inter", opacity: 0.5, fontSize: "0.7rem" }}>*Aplikacja na App Store obecnie nie jest dostępna</span>
+        </div>
+        <div className={styles.Panel__heading__rating}>
+            <Image src="star.svg" width={16} height={16} />
+            <div>
+                <span className={styles.Panel__heading__rating__heading}>4,5 Gwiazdek</span><br />
+                <span className={styles.Panel__heading__rating__description}>na podstawie opinii z Google Play</span>
+            </div>
+        </div>
+    </div>
+}
